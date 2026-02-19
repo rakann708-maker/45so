1ed31e9686e415ec61b9dedbdc096dc87e7c4e08
rakann708-maker <rakann708@gmail.com>
2026-02-19 03:24:53 -0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rakann708-maker/45so/main/SKY.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/rakann708-maker/45so/main/SKY.lua'))()
